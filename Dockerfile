f7538167c8cd96a5c5e92cda802111e150f2bc04
Patrick Oberdorf <patrick@oberdorf.net>
2016-11-18 11:35:48 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -12,9 +12,10 @@ RUN apt-get update && apt-get install -y \
 	debootstrap \
 	man \
 	nano \
+	git \
 	&& pip install diskimage-builder
 
 RUN mkdir -p /usr/local/build/image
+COPY elements/remove-interfaced /usr/local/share/diskimage-builder/elements/remove-interfaced
+COPY elements/chef /usr/local/share/diskimage-builder/elements/chef
 WORKDIR /usr/local/build
-COPY environment environment
-
--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker run -ti -v /dev:/dev -v `pwd`/image:/usr/local/build/image -v `pwd`/cache:/cache --privileged kvm bash -c "source ./environment ; /usr/local/bin/disk-image-create -o image/debian debian vm devuser"
+docker build -t disk-image-create .
+
+docker run -ti \
+	-v /dev:/dev \
+	-v `pwd`/images:/usr/local/build/image \
+	-v `pwd`/cache:/cache \
+	--privileged \
+	--rm \
+	--env-file ./environment \
+	-e DIB_OFFLINE=1 \
+	disk-image-create \
+	bash -c "/usr/local/bin/disk-image-create --offline -o image/debian-`date +'%Y_%m_%d-%H_%M_%S'`-stable debian vm devuser cloud-init-nocloud remove-interfaced chef && cd image && ln -sf debian-`date +'%Y_%m_%d-%H_%M_%S'`-stable.qcow2 debian-current-stable.qcow2"
